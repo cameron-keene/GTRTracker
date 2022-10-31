05c4a4717b8688e8e73237c49a44f2b27265af33
cameron-keene <74378283+cameron-keene@users.noreply.github.com>
2022-10-31 14:43:47 -0400

    Goal Progress reflect actual percentage

--- lib/pages/detailPage.dart
+++ lib/pages/detailPage.dart
@@ -32,118 +32,128 @@ class detailScreen extends StatefulWidget {
 }
 
 class _detailScreenState extends State<detailScreen> {
-
   late GoogleMapController mapController;
-  
-  Set<Marker> _markers = {};
 
-  
+  Set<Marker> _markers = {};
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
   }
+
   @override
   Widget build(BuildContext context) {
     final LatLng _center = LatLng(widget.goal.latitude, widget.goal.longitude);
-    Set<Circle> circles = Set.from([Circle(
-    circleId: CircleId("geofence"),
-    center: LatLng(widget.goal.latitude, widget.goal.longitude),
-    radius: 65,
-    fillColor: Color.fromARGB(92, 43, 121, 194),
-    strokeColor: Color.fromARGB(122, 43, 121, 194),
-)]);
+    Set<Circle> circles = Set.from([
+      Circle(
+        circleId: CircleId("geofence"),
+        center: LatLng(widget.goal.latitude, widget.goal.longitude),
+        radius: 65,
+        fillColor: Color.fromARGB(92, 43, 121, 194),
+        strokeColor: Color.fromARGB(122, 43, 121, 194),
+      )
+    ]);
     // Use the Goal to create the UI.
-     return Scaffold(
+    return Scaffold(
       appBar: AppBar(
         backgroundColor: Color.fromARGB(255, 27, 27, 27),
-        title: Text(widget.goal.name, style: TextStyle(color: Color.fromARGB(255, 43, 121, 194), fontSize: 20)),
+        title: Text(widget.goal.name,
+            style: TextStyle(
+                color: Color.fromARGB(255, 43, 121, 194), fontSize: 20)),
       ),
       backgroundColor: Color.fromARGB(255, 27, 27, 27),
-      
       body: Column(
         mainAxisSize: MainAxisSize.min,
-        children: <Widget> [
-      
-      Center(child:Container(
-        padding: EdgeInsets.fromLTRB(20, 20, 20, 15),
-        child: Align(
-          alignment: Alignment.centerLeft,
-          child:Text('Goal Description',
-            style: GoogleFonts.roboto(
-            color: Color.fromARGB(255, 43, 121, 194),
-            fontSize: 25,
-            fontWeight: FontWeight.bold),)))),
-      
-      //goal description
-      Padding(padding: EdgeInsets.fromLTRB(20, 20, 20, 10),
-        child: Align(
-          alignment: Alignment.centerLeft,
-          child:Text(widget.goal.description ?? "",
-            textAlign: TextAlign.left,
-            style: GoogleFonts.roboto(color: Color.fromARGB(255, 255, 255, 255),
-            fontSize: 20,),
-            )
-          )
-      ),
-  
-      Divider(color: Color.fromARGB(255, 255, 255, 255)),
+        children: <Widget>[
+          Center(
+              child: Container(
+                  padding: EdgeInsets.fromLTRB(20, 20, 20, 15),
+                  child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'Goal Description',
+                        style: GoogleFonts.roboto(
+                            color: Color.fromARGB(255, 43, 121, 194),
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold),
+                      )))),
 
-      Center(child:Container(
-        padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
-        child: Align(
-          alignment: Alignment.centerLeft,
-          child:Text('Goal Progress',
-            style: GoogleFonts.roboto(
-            color: Color.fromARGB(255, 43, 121, 194),
-            fontSize: 25,
-            fontWeight: FontWeight.bold),
-            )
-          )
-        )
-      ),
-      
-      //progress bar
-      Center(child:Container(
-        padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
-        color: Color.fromARGB(132, 56, 56, 56),
-        child:
-          LinearProgressIndicator(
-            minHeight: 7,
-            backgroundColor: Color.fromARGB(255, 255, 255, 255),
-            valueColor: new AlwaysStoppedAnimation<Color>(Color.fromARGB(255, 43, 121, 194)),
-            value: 0.4, ),)), //update with current hours towards goal
-      Divider(color: Color.fromARGB(255, 255, 255, 255)),
-      
-      Center(child:Container(
-        padding: EdgeInsets.fromLTRB(20, 15, 20, 10),
-        child: Align(
-          alignment: Alignment.centerLeft,
-          child:Text('Goal Location',
-            style: GoogleFonts.roboto(
-            color: Color.fromARGB(255, 43, 121, 194),
-            fontSize: 25,
-            fontWeight: FontWeight.bold),
-            )
-          )
-        )
-      ),
-      
-      Expanded(child: Padding(
-        padding: EdgeInsets.all(7.0), 
-        child: GoogleMap(
-        onMapCreated: _onMapCreated,
-        initialCameraPosition: CameraPosition(
-          target: _center,
-          zoom: 18,),
-        circles: circles,
-          ),
-        )
-      ),
-    ],
-        
+          //goal description
+          Padding(
+              padding: EdgeInsets.fromLTRB(20, 20, 20, 10),
+              child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    widget.goal.description ?? "",
+                    textAlign: TextAlign.left,
+                    style: GoogleFonts.roboto(
+                      color: Color.fromARGB(255, 255, 255, 255),
+                      fontSize: 20,
+                    ),
+                  ))),
 
+          Divider(color: Color.fromARGB(255, 255, 255, 255)),
+
+          Center(
+              child: Container(
+                  padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+                  child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'Goal Progress',
+                        style: GoogleFonts.roboto(
+                            color: Color.fromARGB(255, 43, 121, 194),
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold),
+                      )))),
+
+          //progress bar
+          Center(
+              child: Container(
+            padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+            color: Color.fromARGB(132, 56, 56, 56),
+            child: LinearProgressIndicator(
+              minHeight: 7,
+              backgroundColor: Color.fromARGB(255, 255, 255, 255),
+              valueColor: new AlwaysStoppedAnimation<Color>(
+                  Color.fromARGB(255, 43, 121, 194)),
+              value: getPercentage(widget.goal),
+            ),
+          )), //update with current hours towards goal
+          Divider(color: Color.fromARGB(255, 255, 255, 255)),
+
+          Center(
+              child: Container(
+                  padding: EdgeInsets.fromLTRB(20, 15, 20, 10),
+                  child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'Goal Location',
+                        style: GoogleFonts.roboto(
+                            color: Color.fromARGB(255, 43, 121, 194),
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold),
+                      )))),
+
+          Expanded(
+              child: Padding(
+            padding: EdgeInsets.all(7.0),
+            child: GoogleMap(
+              onMapCreated: _onMapCreated,
+              initialCameraPosition: CameraPosition(
+                target: _center,
+                zoom: 18,
+              ),
+              circles: circles,
+            ),
+          )),
+        ],
       ),
-    )
-    ;
+    );
+  }
+
+  double getPercentage(Goal currentGoal) {
+    double percentage = currentGoal.currentDuration / currentGoal.goalDuration;
+    // debugPrint("percentage is: " + percentage.toString());
+    return percentage;
   }
 }
