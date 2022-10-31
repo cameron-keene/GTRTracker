aa907ec52cb11843e3b8746e3298bc86da2dc1ad
Dillan Maraj <dillanmaraj@Dillans-MacBook-Pro.local>
2022-10-31 18:12:40 -0400

    slider that changes map radius

--- lib/pages/goalPage.dart
+++ lib/pages/goalPage.dart
@@ -18,6 +18,7 @@ import 'package:gtrtracker/goalClass/Goal.dart';
 
 // need to hide the location package from the geocoding package
 import 'package:geocoding/geocoding.dart' as test;
+import 'package:gtrtracker/widgets/sliderWidget.dart';
 
 class GoalsPage extends StatefulWidget {
   const GoalsPage({Key? key}) : super(key: key);
@@ -131,7 +132,6 @@ class AddGoalForm extends StatefulWidget {
 }
 
 class _AddGoalFormState extends State<AddGoalForm> {
-
   late GoogleMapController mapController;
   final _nameController = TextEditingController();
   final _descriptionController = TextEditingController();
@@ -141,7 +141,6 @@ class _AddGoalFormState extends State<AddGoalForm> {
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
   }
-  
 
   Future<void> _saveGoal() async {
     // get the current text field contents
@@ -156,7 +155,7 @@ class _AddGoalFormState extends State<AddGoalForm> {
     List<test.Location> locations = await test.locationFromAddress(location);
     latitude = locations.elementAt(0).latitude;
     longitude = locations.elementAt(0).longitude;
-    
+
     print("latitude: $latitude, longitude: $longitude");
     print("starting geoFencing Service");
 
@@ -184,7 +183,6 @@ class _AddGoalFormState extends State<AddGoalForm> {
   }
 
   Future<void> _saveLocation() async {
-
     // get the current text field contents
     final location = _addressController.text;
 
@@ -194,13 +192,15 @@ class _AddGoalFormState extends State<AddGoalForm> {
     List<test.Location> locations = await test.locationFromAddress(location);
     latitude = locations.elementAt(0).latitude;
     longitude = locations.elementAt(0).longitude;
-    
+
     print("latitude: $latitude, longitude: $longitude");
     print("starting geoFencing Service");
 
     Navigator.push(
       context,
-      MaterialPageRoute(builder: (context) => viewGoal(latitude: latitude, longitude: longitude)),
+      MaterialPageRoute(
+          builder: (context) =>
+              viewGoal(latitude: latitude, longitude: longitude)),
     );
 
     // create a new Goal from the form values
@@ -209,7 +209,6 @@ class _AddGoalFormState extends State<AddGoalForm> {
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       appBar: AppBar(
         title: const Text('Add Goal'),
@@ -254,9 +253,8 @@ class _AddGoalFormState extends State<AddGoalForm> {
                     labelStyle: TextStyle(color: Color.fromARGB(255, 0, 0, 0))),
               ),
               ElevatedButton(
-                child: const Text('View Goal Location'),
-                onPressed: _saveLocation
-              ),
+                  child: const Text('View Goal Location'),
+                  onPressed: _saveLocation),
               ElevatedButton(
                 onPressed: _saveGoal,
                 child: const Text('Save'),
@@ -273,56 +271,65 @@ class viewGoal extends StatefulWidget {
   double latitude;
   double longitude;
 
-  viewGoal({Key? key, required this.latitude, required this.longitude}) : super(key: key);
+  viewGoal({Key? key, required this.latitude, required this.longitude})
+      : super(key: key);
 
   @override
   State<viewGoal> createState() => _viewGoalState();
-
 }
 
-  class _viewGoalState extends State<viewGoal> {
+class _viewGoalState extends State<viewGoal> {
+  late GoogleMapController mapController;
+  double rad = 65; //radius for state update with slider
 
-    late GoogleMapController mapController;
-
-      void _onMapCreated(GoogleMapController controller) {
+  void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
   }
 
   @override
   Widget build(BuildContext context) {
     final LatLng _center = LatLng(widget.latitude, widget.longitude);
-    Set<Circle> circles = Set.from([Circle(
-      circleId: CircleId("geofence"),
-      center: LatLng(widget.latitude, widget.longitude),
-      radius: 65,
-      fillColor: Color.fromARGB(92, 43, 121, 194),
-      strokeColor: Color.fromARGB(122, 43, 121, 194),
-    )]);
+    Set<Circle> circles = Set.from([
+      Circle(
+        circleId: CircleId("geofence"),
+        center: LatLng(widget.latitude, widget.longitude),
+        radius: rad,
+        fillColor: Color.fromARGB(92, 43, 121, 194),
+        strokeColor: Color.fromARGB(122, 43, 121, 194),
+      )
+    ]);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Color.fromARGB(255, 27, 27, 27),
-        title: Text("Goal Location", style: TextStyle(color: Color.fromARGB(255, 43, 121, 194), fontSize: 20)),
+        title: Text("Goal Location",
+            style: TextStyle(
+                color: Color.fromARGB(255, 43, 121, 194), fontSize: 20)),
       ),
       backgroundColor: Color.fromARGB(255, 27, 27, 27),
-      
       body: Column(
         mainAxisSize: MainAxisSize.min,
-        children: <Widget> [
-      
-      Expanded(child: Padding(
-        padding: EdgeInsets.all(7.0), 
-        child: GoogleMap(
-        onMapCreated: _onMapCreated,
-        initialCameraPosition: CameraPosition(
-          target: _center,
-          zoom: 18,),
-          circles: circles,
-          ),
-        )
+        children: <Widget>[
+          Expanded(
+              child: Padding(
+            padding: EdgeInsets.all(7.0),
+            child: GoogleMap(
+              onMapCreated: _onMapCreated,
+              initialCameraPosition: CameraPosition(
+                target: _center,
+                zoom: 18,
+              ),
+              circles: circles,
+            ),
+          )),
+          Container(
+              child: Slider(
+            value: rad,
+            min: 20,
+            max: 240,
+            onChanged: (value) => setState(() => rad = value),
+          ))
+        ],
       ),
-    ], 
-      ),
-    )
-    ;
+    );
   }
 }
