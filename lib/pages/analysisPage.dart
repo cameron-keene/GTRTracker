53c313535b53ff4295deb1ccc2095c21baab1ad7
Dillan Maraj <dillanmaraj@Dillans-MacBook-Pro.local>
2022-11-06 12:17:39 -0500

    basic structure

--- lib/pages/analysisPage.dart
+++ lib/pages/analysisPage.dart
@@ -15,6 +15,10 @@ class AnalysisPage extends StatefulWidget {
 class _AnalysisPageState extends State<AnalysisPage> {
   @override
   Widget build(BuildContext context) {
+    final border = RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(15.0),
+    );
+
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Color.fromARGB(255, 27, 27, 27),
@@ -22,48 +26,53 @@ class _AnalysisPageState extends State<AnalysisPage> {
               style: TextStyle(color: Color.fromARGB(255, 43, 121, 194))),
         ),
         backgroundColor: Color.fromARGB(255, 27, 27, 27),
-        body: SingleChildScrollView(
-            child: Column(
-          children: <Widget>[
-            Center(
-                child: Container(
-                    padding: EdgeInsets.fromLTRB(20, 20, 20, 15),
-                    child: Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          'Goal Progress Map',
-                          style: GoogleFonts.roboto(
-                              color: Color.fromARGB(255, 255, 255, 255),
-                              fontSize: 25,
-                              fontWeight: FontWeight.bold),
-                        )))),
-            const locationWidget(),
-            Divider(color: Color.fromARGB(255, 255, 255, 255)),
-            Center(
-                child: Container(
-                    padding: EdgeInsets.fromLTRB(20, 20, 20, 15),
-                    child: Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          'Goal Progress',
-                          style: GoogleFonts.roboto(
-                              color: Color.fromARGB(255, 255, 255, 255),
-                              fontSize: 25,
-                              fontWeight: FontWeight.bold),
-                        )))),
-            SizedBox(
-                height: 75.0,
-                width: 75.0,
-                child: CircularProgressIndicator(
-                  value: 0.25,
-                  backgroundColor: Color.fromARGB(255, 255, 255, 255),
-                  valueColor:
-                      AlwaysStoppedAnimation(Color.fromARGB(255, 43, 121, 194)),
-                  strokeWidth: 12,
-                  semanticsLabel: 'Circular progress indicator',
-                )),
-            Divider(color: Color.fromARGB(255, 255, 255, 255)),
-          ],
-        )));
+        body: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SingleChildScrollView(
+                child: Column(children: <Widget>[
+              Center(
+                  child: Container(
+                      padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
+                      child: Align(
+                          alignment: Alignment.center,
+                          child: Text(
+                            'Trends',
+                            style: GoogleFonts.roboto(
+                                color: Color.fromARGB(255, 255, 255, 255),
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold),
+                          )))),
+              const Padding(
+                  padding: EdgeInsets.fromLTRB(20, 10, 20, 0),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                  )),
+              ListTileTheme(
+                  contentPadding: const EdgeInsets.all(8),
+                  iconColor: Color.fromARGB(255, 0, 0, 0),
+                  textColor: Color.fromARGB(255, 0, 0, 0),
+                  tileColor: Color.fromARGB(255, 255, 255, 255),
+                  style: ListTileStyle.list,
+                  dense: true,
+                  child: ListView.builder(
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      padding: const EdgeInsets.all(8.0),
+                      itemCount: 3,
+                      itemBuilder: (context, index) {
+                        return Card(
+                            child: ListTile(
+                          shape: border,
+                          trailing: Icon(Icons.more_vert),
+                          title: Text(
+                            "First",
+                            textScaleFactor: 1.5,
+                            style: GoogleFonts.roboto(
+                                fontSize: 13, fontWeight: FontWeight.w500),
+                          ),
+                        ));
+                      }))
+            ]))));
   }
 }
