0c97e5ed60e6d1d4c52a23fb053d72af2a97d700
cameron-keene <74378283+cameron-keene@users.noreply.github.com>
2022-11-10 11:12:30 -0500

    changed to dart instead of javascript

--- lib/models/sampleQueries.dart
+++ lib/models/sampleQueries.dart
@@ -2,57 +2,63 @@
 
 
 // -------- Goal Creation --------
-// await DataStore.save(
-//     new Goal({
-// 		"name": "Lorem ipsum dolor sit amet",
-// 		"description": "Lorem ipsum dolor sit amet",
-// 		"goalDuration": 1020,
-// 		"currentDuration": 1020,
-// 		"latitude": 123.45,
-// 		"longitude": 123.45,
-// 		"Activities": [],
-// 		"radius": 1020,
-// 		"productivity": 123.45
-// 	})
-// );
+// final item = Goal(
+// 		name: "Lorem ipsum dolor sit amet",
+// 		description: "Lorem ipsum dolor sit amet",
+// 		goalDuration: 1020,
+// 		currentDuration: 1020,
+// 		latitude: 123.45,
+// 		longitude: 123.45,
+// 		Activities: [],
+// 		radius: 1020,
+// 		productivity: 123.45);
+// await Amplify.DataStore.save(item);
 
 // -------- Goal Update --------
-// /* Models in DataStore are immutable. To update a record you must use the copyOf function
-//  to apply updates to the item’s fields rather than mutating the instance directly */
-// await DataStore.save(Goal.copyOf(CURRENT_ITEM, item => {
-//     // Update the values on {item} variable to update DataStore entry
-// }));
+// final updatedItem = item.copyWith(
+// 		name: "Lorem ipsum dolor sit amet",
+// 		description: "Lorem ipsum dolor sit amet",
+// 		goalDuration: 1020,
+// 		currentDuration: 1020,
+// 		latitude: 123.45,
+// 		longitude: 123.45,
+// 		Activities: [],
+// 		radius: 1020,
+// 		productivity: 123.45);
+// await Amplify.DataStore.save(updatedItem);
 
 
 // -------- Goal Delete --------
-// const modelToDelete = await DataStore.query(Goal, 123456789);
-// DataStore.delete(modelToDelete);
+// await Amplify.DataStore.delete(toDeleteItem);
 
-// -------- Goal Creation --------
-// const models = await DataStore.query(Goal);
-// console.log(models);
+// -------- Goal Query --------
+// try {
+//   List<Goal> Goals = await Amplify.DataStore.query(Goal.classType);
+// } catch (e) {
+//   print("Could not query DataStore: " + e);
+// }
 
 
 // -------- GeoActivity Creation --------
-// await DataStore.save(
-//     new GeoActivity({
-// 		"goalID": "a3f4095e-39de-43d2-baf4-f8c16f0f6f4d",
-// 		"activityTime": "1970-01-01T12:30:23.999Z",
-// 		"duration": 1020
-// 	})
-// );
+// final item = GeoActivity(
+// 		goalID: "a3f4095e-39de-43d2-baf4-f8c16f0f6f4d",
+// 		activityTime: TemporalDateTime.fromString("1970-01-01T12:30:23.999Z"),
+// 		duration: 1020);
+// await Amplify.DataStore.save(item);
 
 // -------- GeoActivity Update --------
-/* Models in DataStore are immutable. To update a record you must use the copyOf function
- to apply updates to the item’s fields rather than mutating the instance directly */
-// await DataStore.save(GeoActivity.copyOf(CURRENT_ITEM, item => {
-//     // Update the values on {item} variable to update DataStore entry
-// }));
+// final updatedItem = item.copyWith(
+// 		goalID: "a3f4095e-39de-43d2-baf4-f8c16f0f6f4d",
+// 		activityTime: TemporalDateTime.fromString("1970-01-01T12:30:23.999Z"),
+// 		duration: 1020);
+// await Amplify.DataStore.save(updatedItem);
 
 // -------- GeoActivity Delete --------
-// const modelToDelete = await DataStore.query(GeoActivity, 123456789);
-// DataStore.delete(modelToDelete);
+// await Amplify.DataStore.delete(toDeleteItem);
 
 // -------- GeoActivity Query --------
-// const models = await DataStore.query(GeoActivity);
-// console.log(models);
+// try {
+//   List<GeoActivity> GeoActivitys = await Amplify.DataStore.query(GeoActivity.classType);
+// } catch (e) {
+//   print("Could not query DataStore: " + e);
+// }
