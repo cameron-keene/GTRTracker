769d2ec29b6083ee0e7efe4dd7ee30ea1cf99043
Dillan Maraj <dillanmaraj@Dillans-MacBook-Pro.local>
2022-11-11 15:31:45 -0500

    future builder

--- lib/pages/analysisPage.dart
+++ lib/pages/analysisPage.dart
@@ -24,18 +24,21 @@ class AnalysisPage extends StatefulWidget {
 class _AnalysisPageState extends State<AnalysisPage> {
   List<Goal> goals = [];
 
-  void getGoals() async {
+  Future<List<Goal>> getGoals() async {
     try {
       goals = await Amplify.DataStore.query(Goal.classType);
+      // setState(() {
+      //   goals = yuh;
+      // });
       print("numgoals: " + goals.length.toString());
     } catch (e) {
       print("Could not query DataStore: " + e.toString());
     }
+    return goals;
   }
 
   @override
   Widget build(BuildContext context) {
-    getGoals();
     final border = RoundedRectangleBorder(
       borderRadius: BorderRadius.circular(15.0),
     );
@@ -48,136 +51,182 @@ class _AnalysisPageState extends State<AnalysisPage> {
         body: Padding(
             padding: const EdgeInsets.all(8.0),
             child: SingleChildScrollView(
-                child: Column(children: <Widget>[
-              Container(
-                child: Text(
-                  goals[0].toString(),
-                  style: GoogleFonts.roboto(
-                      color: Color.fromARGB(255, 255, 255, 255),
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold),
-                ),
-              ),
-              Center(
-                  child: Container(
-                      padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
-                      child: Align(
-                          alignment: Alignment.center,
-                          child: Text(
-                            'Trends',
-                            style: GoogleFonts.roboto(
-                                color: Color.fromARGB(255, 255, 255, 255),
-                                fontSize: 25,
-                                fontWeight: FontWeight.bold),
-                          )))),
-              const Padding(
-                  padding: EdgeInsets.fromLTRB(20, 10, 20, 0),
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                  )),
-              Container(
-                color: Color.fromARGB(255, 255, 255, 255),
-                margin: const EdgeInsets.only(top: 10),
-                width: 350,
-                height: 300,
-                child: Chart(
-                  data: timeSeriesSales,
-                  variables: {
-                    'time': Variable(
-                      accessor: (TimeSeriesSales datum) => datum.time,
-                      scale: TimeScale(
-                        formatter: (time) => _monthDayFormat.format(time),
-                      ),
-                    ),
-                    'sales': Variable(
-                      accessor: (TimeSeriesSales datum) => datum.sales,
-                    ),
-                  },
-                  elements: [
-                    LineElement(
-                      shape: ShapeAttr(value: BasicLineShape(dash: [5, 2])),
-                      selected: {
-                        'touchMove': {1}
-                      },
-                    )
-                  ],
-                  coord: RectCoord(color: const Color(0xffdddddd)),
-                  axes: [
-                    Defaults.horizontalAxis,
-                    Defaults.verticalAxis,
-                  ],
-                  selections: {
-                    'touchMove': PointSelection(
-                      on: {
-                        GestureType.scaleUpdate,
-                        GestureType.tapDown,
-                        GestureType.longPressMoveUpdate
-                      },
-                      dim: Dim.x,
-                    )
-                  },
-                  tooltip: TooltipGuide(
-                    followPointer: [false, true],
-                    align: Alignment.topLeft,
-                    offset: const Offset(-20, -20),
-                  ),
-                  crosshair: CrosshairGuide(followPointer: [false, true]),
-                ),
-              ),
-              const Padding(
-                  padding: EdgeInsets.fromLTRB(20, 10, 20, 0),
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                  )),
-              Container(
-                color: Color.fromARGB(255, 255, 255, 255),
-                margin: const EdgeInsets.only(top: 10),
-                width: 350,
-                height: 300,
-                child: Chart(
-                  data: timeSeriesSales,
-                  variables: {
-                    'time': Variable(
-                      accessor: (TimeSeriesSales datum) => datum.time,
-                      scale: TimeScale(
-                        formatter: (time) => _monthDayFormat.format(time),
-                      ),
-                    ),
-                    'sales': Variable(
-                      accessor: (TimeSeriesSales datum) => datum.sales,
-                    ),
-                  },
-                  elements: [
-                    LineElement(
-                      shape: ShapeAttr(value: BasicLineShape(dash: [5, 2])),
-                      selected: {
-                        'touchMove': {1}
-                      },
-                    )
-                  ],
-                  coord: RectCoord(color: const Color(0xffdddddd)),
-                  axes: [
-                    Defaults.horizontalAxis,
-                    Defaults.verticalAxis,
-                  ],
-                  selections: {
-                    'touchMove': PointSelection(
-                      on: {
-                        GestureType.scaleUpdate,
-                        GestureType.tapDown,
-                        GestureType.longPressMoveUpdate
-                      },
-                      dim: Dim.x,
-                    )
-                  },
-                  tooltip: TooltipGuide(
-                    followPointer: [false, true],
-                    align: Alignment.topLeft,
-                    offset: const Offset(-20, -20),
-                  ),
-                  crosshair: CrosshairGuide(followPointer: [false, true]),
-                ),
-              ),
-            ]))));
+                child: FutureBuilder<List<Goal>>(
+                    future: getGoals(),
+                    builder: (BuildContext context,
+                        AsyncSnapshot<List<Goal>> snapshot) {
+                      List<Widget> children;
+
+                      if (snapshot.connectionState == ConnectionState.done) {
+                        try {
+                          children = <Widget>[
+                            Container(
+                              child: Text(
+                                snapshot.data![0].toString(),
+                                style: GoogleFonts.roboto(
+                                    color: Color.fromARGB(255, 255, 255, 255),
+                                    fontSize: 25,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            Center(
+                                child: Container(
+                                    padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
+                                    child: Align(
+                                        alignment: Alignment.center,
+                                        child: Text(
+                                          'Trends',
+                                          style: GoogleFonts.roboto(
+                                              color: Color.fromARGB(
+                                                  255, 255, 255, 255),
+                                              fontSize: 25,
+                                              fontWeight: FontWeight.bold),
+                                        )))),
+                            const Padding(
+                                padding: EdgeInsets.fromLTRB(20, 10, 20, 0),
+                                child: Align(
+                                  alignment: Alignment.centerLeft,
+                                )),
+                            Container(
+                              color: Color.fromARGB(255, 255, 255, 255),
+                              margin: const EdgeInsets.only(top: 10),
+                              width: 350,
+                              height: 300,
+                              child: Chart(
+                                data: timeSeriesSales,
+                                variables: {
+                                  'time': Variable(
+                                    accessor: (TimeSeriesSales datum) =>
+                                        datum.time,
+                                    scale: TimeScale(
+                                      formatter: (time) =>
+                                          _monthDayFormat.format(time),
+                                    ),
+                                  ),
+                                  'sales': Variable(
+                                    accessor: (TimeSeriesSales datum) =>
+                                        datum.sales,
+                                  ),
+                                },
+                                elements: [
+                                  LineElement(
+                                    shape: ShapeAttr(
+                                        value: BasicLineShape(dash: [5, 2])),
+                                    selected: {
+                                      'touchMove': {1}
+                                    },
+                                  )
+                                ],
+                                coord:
+                                    RectCoord(color: const Color(0xffdddddd)),
+                                axes: [
+                                  Defaults.horizontalAxis,
+                                  Defaults.verticalAxis,
+                                ],
+                                selections: {
+                                  'touchMove': PointSelection(
+                                    on: {
+                                      GestureType.scaleUpdate,
+                                      GestureType.tapDown,
+                                      GestureType.longPressMoveUpdate
+                                    },
+                                    dim: Dim.x,
+                                  )
+                                },
+                                tooltip: TooltipGuide(
+                                  followPointer: [false, true],
+                                  align: Alignment.topLeft,
+                                  offset: const Offset(-20, -20),
+                                ),
+                                crosshair: CrosshairGuide(
+                                    followPointer: [false, true]),
+                              ),
+                            ),
+                            const Padding(
+                                padding: EdgeInsets.fromLTRB(20, 10, 20, 0),
+                                child: Align(
+                                  alignment: Alignment.centerLeft,
+                                )),
+                            Container(
+                              color: Color.fromARGB(255, 255, 255, 255),
+                              margin: const EdgeInsets.only(top: 10),
+                              width: 350,
+                              height: 300,
+                              child: Chart(
+                                data: timeSeriesSales,
+                                variables: {
+                                  'time': Variable(
+                                    accessor: (TimeSeriesSales datum) =>
+                                        datum.time,
+                                    scale: TimeScale(
+                                      formatter: (time) =>
+                                          _monthDayFormat.format(time),
+                                    ),
+                                  ),
+                                  'sales': Variable(
+                                    accessor: (TimeSeriesSales datum) =>
+                                        datum.sales,
+                                  ),
+                                },
+                                elements: [
+                                  LineElement(
+                                    shape: ShapeAttr(
+                                        value: BasicLineShape(dash: [5, 2])),
+                                    selected: {
+                                      'touchMove': {1}
+                                    },
+                                  )
+                                ],
+                                coord:
+                                    RectCoord(color: const Color(0xffdddddd)),
+                                axes: [
+                                  Defaults.horizontalAxis,
+                                  Defaults.verticalAxis,
+                                ],
+                                selections: {
+                                  'touchMove': PointSelection(
+                                    on: {
+                                      GestureType.scaleUpdate,
+                                      GestureType.tapDown,
+                                      GestureType.longPressMoveUpdate
+                                    },
+                                    dim: Dim.x,
+                                  )
+                                },
+                                tooltip: TooltipGuide(
+                                  followPointer: [false, true],
+                                  align: Alignment.topLeft,
+                                  offset: const Offset(-20, -20),
+                                ),
+                                crosshair: CrosshairGuide(
+                                    followPointer: [false, true]),
+                              ),
+                            ),
+                          ];
+                        } catch (e) {
+                          children = [Text(e.toString())];
+                        }
+                      } else if (snapshot.hasError) {
+                        children = <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.only(top: 16),
+                            child: Text('Error: ${snapshot.error}'),
+                          )
+                        ];
+                      } else {
+                        children = const <Widget>[
+                          Padding(
+                            padding: EdgeInsets.only(top: 16),
+                            child: Text('Awaiting result... (dummy data)'),
+                          )
+                        ];
+                      }
+                      return Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: children,
+                        ),
+                      );
+                    }))));
   }
 }
